78866cc0195dfe3d13e94060117c4938685588a4
annaglova <anna.maliyenko@gmail.com>
2025-07-07 15:44:11 +0300

    feat: implement HideCursor component to hide cursor on landing pages

--- apps/landing/src/layouts/LandingLayout.tsx
+++ apps/landing/src/layouts/LandingLayout.tsx
@@ -2,6 +2,7 @@
 
 import Footer from "@/components/Footer";
 import LandingMenu from "@/components/LandingMenu";
+import HideCursor from "@/components/HideCursor";
 import React from "react";
 
 interface LandingLayoutProps {
@@ -10,7 +11,8 @@ interface LandingLayoutProps {
 
 export default function LandingLayout({ children }: LandingLayoutProps) {
   return (
-    <div className="min-h-screen flex flex-col w-full">
+    <div className="min-h-screen flex flex-col w-full no-cursor">
+      <HideCursor />
       <LandingMenu />
       <main className="flex-1 flex flex-col ">{children}</main>
       <Footer />
